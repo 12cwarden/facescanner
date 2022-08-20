20d8c1ec339ee3a832a9a6e7d0e55a3f40384be3
Cory <12cwarden@gmail.com>
2022-08-20 13:17:06 -0400

    fix

--- src/containers/App.js
+++ src/containers/App.js
@@ -94,7 +94,7 @@ class App extends Component {
       .then((response) => response.json())
       .then((response) => {
         if (response) {
-          fetch("http://https://rocky-gorge-61910.herokuapp.com:3000/image", {
+          fetch("http://https://rocky-gorge-61910.herokuapp.com/image", {
             method: "put",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
